a4d526d0081c1f2abb91c7a1a4dba43896261b93
Osamu-Yamamoto <40651102+Osamu-Yamamoto@users.noreply.github.com>
2020-06-11 10:07:05 +0800

    sync

--- log_manager.py
+++ log_manager.py
@@ -5,8 +5,15 @@ import os
 class Error_Handler():
 
     def __init__(self):
-        logging.basicConfig(filename='/home/pi/PycharmProjects/untitled/app.log', filemode = 'a', format = '%(name)s - $(levelname)s - %(message)s')
+        logging.basicConfig(filename='log/app.log', filemode = 'a', format = '%(name)s - $(levelname)s - %(message)s')
         logging.basicConfig(level = logging.DEBUG)
         self.logger = logging.getLogger('logger')
 
-    def
+    def get_logger(self):
+        return self.logger
+
+    def report_issue(self, message, file = None):
+        raise NotImplementedError
+
+
+
